980963b98e2192cb9f3bb97c363dbc5991091f9e
Luis-37 <111667777+Luis-37@users.noreply.github.com>
2022-10-11 10:45:11 -0500

    Add files via upload

--- ADT_Lista_Ligada_T5/src/fes/aragon/Main.java
+++ ADT_Lista_Ligada_T5/src/fes/aragon/Main.java
@@ -8,11 +8,11 @@ public class Main {
 		head.agregarAlFinal(20);
 		head.agregarAlFinal(30);
 		head.agregarAlFinal(10);
-		System.out.println(head.estaVacio());
 		head.agregarAlInicio(40);
+		System.out.println(head.estaVacio());
 		head.agregarDespuesDe(30, 3);
-		head.eliminar(3);
 		head.eliminarElPrimero();
+		head.eliminar(3);
 		head.eliminarElFinal();
 		System.out.println(head.buscar(20));
 		head.actualizar(40, 50);
